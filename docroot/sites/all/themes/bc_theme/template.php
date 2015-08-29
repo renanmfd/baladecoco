66422030017dea1b6e254d742620d44a7b5e8052
Renan Dias <renan@wearepropeople.com.br>
2015-08-29 18:35:52 -0300

    MBC-008: Update and templates for menus 4

--- docroot/sites/all/themes/bc_theme/template.php
+++ docroot/sites/all/themes/bc_theme/template.php
@@ -11,30 +11,37 @@
 function bc_theme_menu_tree($vars) {
   // Override default THEME_menu_tree to add class with the menu title.
   $title = str_replace('menu_tree__', '', $vars['theme_hook_original']);
-  return '<ul class="menu ' . drupal_html_class($title) . '">' . $vars ['tree'] . '</ul>';
+  return '<ul class="menu ' . drupal_html_class($title) . '">' . $vars['tree'] . '</ul>';
 }
 
 /**
- * 
+ * Implement HOOK_menu_link__MENUID() for main-menu.
  */
 function bc_theme_menu_link__main_menu($vars) {
   // Theme main-menu itens to add icons with span wrappers. The icons come
   // from the ID attribute entered in the admin menu interface.
-  $element = $vars ['element'];
+  $element = $vars['element'];
   $sub_menu = '';
 
-  if ($element ['#below']) {
+  if ($element['#below']) {
     $sub_menu = drupal_render($element ['#below']);
   }
 
   $link = '';
-  if (!empty($element ['#attributes']['id'])) {
-    $link = '<span class="' . $element ['#attributes']['id'] . '"></span>';
-    unset($element ['#attributes']['id']);
+  if (!empty($element['#attributes']['id'])) {
+    $link = '<span class="' . $element['#attributes']['id'] . '"></span>';
+    unset($element['#attributes']['id']);
   }
 
-  $element ['#localized_options']['html'] = TRUE;
-  $link .= '<span class="title">' . $element ['#title'] . '</span>';
-  $output = l($link, $element ['#href'], $element ['#localized_options']);
-  return '<li' . drupal_attributes($element ['#attributes']) . '>' . $output . $sub_menu . "</li>\n";
+  $element['#localized_options']['html'] = TRUE;
+  $link .= '<span class="title">' . $element['#title'] . '</span>';
+  $output = l($link, $element ['#href'], $element['#localized_options']);
+  return '<li' . drupal_attributes($element['#attributes']) . '>' . $output . $sub_menu . "</li>\n";
+}
+
+/**
+ * Implement HOOK_menu_link__MENUID() for user-menu.
+ */
+function bc_theme_menu_link__user_menu($vars) {
+  return bc_theme_menu_link__main_menu($vars);
 }
