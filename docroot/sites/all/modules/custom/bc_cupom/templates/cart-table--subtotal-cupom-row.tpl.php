3c1df6c12849138f9dd8c2cf1a69e707388a85fe
Renan Dias <renan@wearepropeople.com.br>
2015-09-14 14:56:17 -0300

    MBC-53: Install bc_cupom

---
+++ docroot/sites/all/modules/custom/bc_cupom/templates/cart-table--subtotal-cupom-row.tpl.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * @file
+ * Template for user CART SUBTOTAL row.
+ *
+ * See template_preprocess_cart_table_subtotal_row() in bc_cart.module.
+ */
+
+?>
+
+<div class="row subtotal cart-<?php print $cart['cid']; ?>">
+
+  <?php /** CUPOM **/ ?>
+  <div class="subtotal__cupom">
+    <?php print render($cupom_form); ?>
+  </div>
+
+  <?php /** SUB-TOTAL WITH CUPOM **/ ?>
+  <?php if ($cupom_isset): ?>
+    <div class="subtotal__value">
+
+      <div class="label"><?php print t('Sub-total:'); ?></div>
+
+      <div class="subtotal__value__nocupom">
+        <span class="value"><?php print $subtotal; ?></span>
+        <span class="currency">R$</span>
+      </div>
+
+      <div class="subtotal__value__discount">
+        <span class="value"><?php print $discount; ?></span>
+        <span class="currency">R$</span>
+        <span class="currency percentage">(<?php print $percetage; ?>%)</span>
+      </div>
+
+      <div class="subtotal__value__cupom">
+        <span class="value"><?php print $subtotal_cupom; ?></span>
+        <span class="currency">R$</span>
+      </div>
+
+    </div>
+  <?php /** SUB-TOTAL WITHOUT CUPOM **/ ?>
+  <?php else: ?>
+    <div class="subtotal__value">
+      <span class="label"><?php print t('Sub-total:'); ?></span>
+      <span class="value"><?php print $subtotal; ?></span>
+      <span class="currency">R$</span>
+    </div>
+  <?php endif; ?>
+
+</div>
+
