c8985e8c60330b6cacf2fd82790c6bf58311277d
Renan Dias <renanmfd@gmail.com>
2017-03-14 00:33:31 -0300

    Add htaccess

--- sites/default/prod.settings.php
+++ sites/default/prod.settings.php
@@ -332,7 +332,7 @@ ini_set('session.cookie_lifetime', 2000000);
  * between your various domains. Make sure to always start the $cookie_domain
  * with a leading dot, as per RFC 2109.
  */
-$cookie_domain = '.meninadasbalas.com.br';
+# $cookie_domain = '.meninadasbalas.com.br';
 
 /**
  * Variable overrides:
